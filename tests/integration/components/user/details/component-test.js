6b5303ad2aa5eeaea90d57642f0216ca1d010327
Igor Juraszek <igorjuraszek@gmail.com>
2022-07-02 15:48:51 +0200

    Adding packages and tests

--- tests/integration/components/user/details/component-test.js
+++ tests/integration/components/user/details/component-test.js
@@ -2,25 +2,26 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { faker } from '@faker-js/faker';
 
 module('Integration | Component | user/details', function (hooks) {
   setupRenderingTest(hooks);
 
-  test.skip('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders', async function (assert) {
+    faker.setLocale('pl');
+    const user = {
+      id: '1',
+      username: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      photoURL: faker.image.avatar(),
+    };
+    this.set('user', user);
+    await render(hbs`<User::Details @user={{this.user}}/>`);
 
-    await render(hbs`<User::Details />`);
-
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <User::Details>
-        template block text
-      </User::Details>
-    `);
-
-    assert.dom(this.element).hasText('template block text');
+    assert.dom('[data-test-id]').hasText(user.id);
+    assert.dom('[data-test-username]').hasText(user.username);
+    assert.dom('[data-test-email]').hasText(user.email);
+    assert.dom('[data-test-password]').hasText(user.password);
   });
 });
